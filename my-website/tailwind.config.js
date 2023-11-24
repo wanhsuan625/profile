cab9c7d72238e550d6f505cc8e4431d5f8c3dcb6
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-11-24 13:50:13 +0800

    Docs : add usual color

--- my-website/tailwind.config.js
+++ my-website/tailwind.config.js
@@ -3,9 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens:{
+        'xs': '480px',
+      },
       spacing:{
-        '60':'240px',
-        '80':'320px',
+        '5.5': '22px',
+        '13': '52px',
+        '15': '60px',
+        '17': '68px',
+        '18': '72px',
+        '65': '260px',
+        '100': '400px',
+        '110': '440px',
       },
       colors: {
         'pink': '#D46A98',
@@ -14,6 +23,31 @@ module.exports = {
       },
       transitionProperty: {
         'width': 'width',
+        'max-w': 'max-width',
+      },
+      width: {
+        '62.5': '250px',
+      },
+      fontSize: {
+        '3xl': ['2rem', {
+          lineHeight: '2.5rem',
+          letterSpacing: '0em',
+          fontWeight: '700',
+        }],
+        '4xl': ['2.5rem', {
+          lineHeight: '2.75rem',
+          letterSpacing: '0em',
+          fontWeight: '700',
+        }]
+      },
+      keyframes: {
+        'bounce': {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(40%)' },
+        }
+      },
+      minHeight: {
+        '18' : '72px',
       }
     },
   },
