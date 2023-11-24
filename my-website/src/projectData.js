49f373f959f16c945467987b71914248d3f5d539
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-11-24 13:38:24 +0800

    Docs: add each project data

---
+++ my-website/src/projectData.js
@@ -0,0 +1,113 @@
+import cover_qrCode from './images/cover-project-1-qrCode.png';
+import cover_productPrevieCard from './images/cover-project-2-productPrevieCard.png';
+import cover_resultSummary from './images/cover-project-3-resultSummary.png';
+import cover_skilledElearning from './images/cover-project-4-skilledElearning.png';
+import cover_podRequestAcess from './images/cover-project-5-podRequestAcess.png';
+import cover_faqAccordingCard from './images/cover-project-6-faqAccordingCard.png';
+import cover_newletterSignUp from './images/cover-project-7-newletterSignUp.png';
+import cover_singlePageDesign from './images/cover-project-8-singlePageDesign.png';
+import cover_githubUserSearch from './images/cover-project-9-githubUserSearch.png';
+
+import cover_taipeiDayTrip from './images/cover-project-11-taipeiDayTrip.png';
+
+export const projects =  [
+    {
+        "id" :  "1",
+        "title" : "QR Code",
+        "description" : "QR code component",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/1.%20qr-code-component-main/index.html?_blank",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/1.%20qr-code-component-main",
+        "cover" : cover_qrCode,
+        "skill" : [ "HTML" , "CSS" , "Component" ]          
+    },
+    {
+        "id" :  "2",
+        "title" : "Product Preview Card",
+        "description" : "Prodect preview card component",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/2.%20product-preview-card-component-main/index.html?_blank",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/2.%20product-preview-card-component-main",
+        "cover" : cover_productPrevieCard,
+        "skill":[ "HTML" , "Sass/SCSS" , "Component" ]
+    },
+    {
+        "id" :  "3",
+        "title" : "Results Summary",
+        "description" : "Results summary component",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/3.%20results-summary-component-main/index.html?_blank",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/3.%20results-summary-component-main",
+        "cover" : cover_resultSummary,
+        "skill":[ "HTML" , "Sass/SCSS" , "Component"]
+    },
+    {
+        "id" :  "4",
+        "title" : "Skilled Elearning",
+        "description" : "Landing page for imaginary elearning",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/4.%20skilled-elearning-landing-page/starter-code/index.html?_blank",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/4.%20skilled-elearning-landing-page",
+        "cover" : cover_skilledElearning,
+        "skill":[ "HTML" , "Sass/SCSS" , "Landing page" ]
+    },
+    {
+        "id" :  "5",
+        "title" : "Pod Request Access",
+        "description" : "A landing page for imaginary Pod music, and setting email validation test.",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/5.%20pod-request-access-landing-page/starter-code/index.html",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/5.%20pod-request-access-landing-page",
+        "cover" : cover_podRequestAcess,
+        "skill":[ "HTML" , "Sass/SCSS" , "JavaScript" , "Landing page" ]
+    },
+    {
+        "id" :  "6",
+        "title" : "FAQ According Card",
+        "description" : "FAQ accoding card component",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/6.%20faq-accordion-card-main/index.html",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/6.%20faq-accordion-card-main",
+        "cover" : cover_faqAccordingCard,
+        "skill":[ "HTML" , "Sass/SCSS"  , "Component" ]
+    },
+    {
+        "id" :  "7",
+        "title" : "Newletter Sign Up",
+        "description" : "A landing page that redirects to the success page upon email verification.",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/7.newsletter-sign-up-with-success-message-main/index.html?_blank",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/7.newsletter-sign-up-with-success-message-main",
+        "cover" : cover_newletterSignUp,
+        "skill":[ "HTML" , "Sass/SCSS" , "JavaScript" , "Landing page" ]
+    },
+    {
+        "id" :  "8",
+        "title" : "Single Page Design",
+        "description" : "A landing page with slider and layout with CSS grid",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/8.single-page-design-portfolio/index.html",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/8.single-page-design-portfolio",
+        "cover" : cover_singlePageDesign,
+        "skill":[ "HTML" , "Sass/SCSS" , "Landing page" ]
+    },
+    {
+        "id" :  "9",
+        "title" : "Github User Search",
+        "description" : "Implement GitHub user search using the GitHub API and incorporate a dark/light theme switch.",
+        "liveSite" : "https://wanhsuan625.github.io/frontend-mentor/9.github-user-search-app/index.html",
+        "codePage" : "https://github.com/wanhsuan625/frontend-mentor/tree/main/9.github-user-search-app",
+        "cover" : cover_githubUserSearch,
+        "skill":[ "HTML" , "Sass/SCSS" , "JavaScript" , "Landing page" , "API" ]
+    },
+    {
+        "id" :  "10",
+        "title" : "Bookmark Page",
+        "description" : "",
+        "liveSite" : "",
+        "codePage" : "",
+        "cover" : "",
+        "skill":[]
+    },
+    {
+        "id" :  "11",
+        "title" : "Taipei Day Trip",
+        "description" : "An e-commerce website which enables you to discover several famous attractions in Taipei ",
+        "liveSite" : "http://54.199.123.84:3000/",
+        "codePage" : "https://github.com/wanhsuan625/taipei-day-trip",
+        "cover" : cover_taipeiDayTrip,
+        "skill":[ "HTML" , "CSS" , "JavaScript" , "API" , "Multi Page" , "Full Stack" ]
+    }
+]
