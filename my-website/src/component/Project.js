48674247b29ffbf5fce6bf500e8c49ddfbfefe34
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-11-24 17:07:11 +0800

    Style: modify the size of liveSite and CodePage icons

--- my-website/src/component/Project.js
+++ my-website/src/component/Project.js
@@ -10,14 +10,14 @@ function Project( { cover , title , description , liveSite , codePage } ){
             
             <div className='text-black p-4 pt-7 md:p-6 md:pt-7 md:pb-5'>
                 <h1 className='font-medium text-xl lg:text-2xl'>{title}</h1>
-                <p className='font-extralight mt-2 mb-3 min-h-18'>{description}</p>
+                <p className='font-extralight mt-2 mb-4 min-h-18'>{description}</p>
 
                 <div className='flex justify-end items-center gap-6'>
                     <a href={liveSite} target='_blank'>
-                        <Livesite className='w-5 h-5 cursor-pointer'/>
+                        <Livesite className='w-5 h-5 lg:w-6 lg:h-6 cursor-pointer'/>
                     </a>
                     <a href={codePage} target='_blank'>
-                        <CodePage className='w-5.5 h-5.5 cursor-pointer'/>
+                        <CodePage className='w-6 h-6 lg:w-7 lg:h-7 cursor-pointer'/>
                     </a>
                 </div>
             </div>
