e015c9d4b6219bbcee0678c1253b40b297326633
wanhsuan625 <54431672+wanhsuan625@users.noreply.github.com>
2023-11-30 23:37:16 +0800

    Feat: apply footer section in App.js

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -3,6 +3,8 @@ import Navbar from './section/Navbar';
 import Header from './section/Header';
 import ProjectContainer from './section/ProjectContainer';
 import About from './section/About';
+import Contact from './section/Contact';
+import Footer from './section/Footer';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
     <Header />
     <ProjectContainer />
     <About />
+    <Contact />
+    <Footer />
   </>
   )
 }
